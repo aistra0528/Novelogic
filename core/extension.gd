4c7cc7eadf62c90d0a8362c021ec33ca3adc3d99
Aistra <aistra0528@foxmail.com>
2025-12-15 14:40:55 +0800

    remove NovelogicExtension.draw()

--- core/extension.gd
+++ core/extension.gd
@@ -1,32 +1,9 @@
 class_name NovelogicExtension
 extends Node
 
-var _decks := { }
-var _last_cards := { }
-
-
 func wait(sec: float):
 	return Novelogic.get_tree().create_timer(sec).timeout
 
 
 func d(to: int) -> int:
 	return randi_range(1, to)
-
-
-func draw(...deck: Array) -> Variant:
-	var key := deck.duplicate()
-	if key not in _decks:
-		deck.shuffle()
-		_decks[key] = deck
-		_last_cards[key] = deck.back()
-	if _decks[key].is_empty():
-		deck.shuffle()
-		while is_same(_last_cards[key], deck.front()):
-			deck.shuffle()
-		_decks[key] = deck
-		_last_cards[key] = deck.back()
-	return _decks[key].pop_front()
-
-
-func drawi(n: int) -> int:
-	return draw(range(n))
