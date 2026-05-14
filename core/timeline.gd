a6dacd587f7ff427ac8cad55c072fecd585a3b40
Aistra <aistra0528@foxmail.com>
2026-05-14 12:21:02 +0800

    add NovelogicTimeline.from_array

--- core/timeline.gd
+++ core/timeline.gd
@@ -6,33 +6,32 @@ var stack: PackedInt32Array = []
 var variables := { }
 
 
-static func from_file(path: String, include_type: Array = []) -> NovelogicTimeline:
-	var file := FileAccess.open(path, FileAccess.READ)
-	if not file:
-		return null
-
-	var lines := PackedStringArray()
-	while file.get_position() < file.get_length():
-		lines.append(file.get_line())
-
+static func from_array(array: PackedStringArray, path: String, include_type: Array = []) -> NovelogicTimeline:
 	var events: Array[TimelineEvent] = []
 	var i := 0
-	while i < lines.size():
-		var event := TimelineEvent.create(lines[i], include_type)
+	while i < array.size():
+		var event := TimelineEvent.create(array[i], include_type)
 		if not event:
 			i += 1
 			continue
 		event.start_line = i + 1
-		event.lines.append(lines[i])
+		event.lines.append(array[i])
 		i += 1
-
-		while i < lines.size() and event.is_multiline(lines[i]):
-			event.lines.append(lines[i])
+		while i < array.size() and event.is_multiline(array[i]):
+			event.lines.append(array[i])
 			i += 1
-
 		events.append(event)
-
 	var timeline := NovelogicTimeline.new()
 	timeline.path = path
 	timeline.events = events
 	return timeline
+
+
+static func from_file(path: String, include_type: Array = []) -> NovelogicTimeline:
+	var file := FileAccess.open(path, FileAccess.READ)
+	if not file:
+		return null
+	var array := PackedStringArray()
+	while file.get_position() < file.get_length():
+		array.append(file.get_line())
+	return from_array(array, path, include_type)
